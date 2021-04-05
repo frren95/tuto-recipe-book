d16f2105c9beaad9d523fead6caf836cf53a59b0
Frédérique <30723124+frren95@users.noreply.github.com>
2021-04-05 13:54:08 +0200

    Recipe Model + added main list building

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://tse3.mm.bing.net/th?id=OIP.dUtSVIi6aDDgX0sBNjy96QAAAA&pid=Api'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://tse3.mm.bing.net/th?id=OIP.dUtSVIi6aDDgX0sBNjy96QAAAA&pid=Api'),
+  ];
 
   constructor() { }
 
